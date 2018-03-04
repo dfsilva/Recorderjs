1b06c56607c9db67adda7523acd4db968a3ebdab
diego <diegosiuniube@gmail.com>
2018-03-04 18:20:58 -0300

    alteracoes

--- recorder.js
+++ recorder.js
@@ -1,90 +1,144 @@
-"use strict";
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["Recorder"] = factory();
+	else
+		root["Recorder"] = factory();
+})(typeof self !== 'undefined' ? self : this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
 
+"use strict";
+/* WEBPACK VAR INJECTION */(function(global) {
+
+var AudioContext = global.AudioContext || global.webkitAudioContext;
+
+
+// Constructor
 var Recorder = function( config ){
 
   if ( !Recorder.isRecordingSupported() ) {
-    throw "Recording is not supported in this browser";
+    throw new Error("Recording is not supported in this browser");
   }
-
-  this.config = config = config || {};
-  this.config.command = "init";
-  this.config.bufferLength = config.bufferLength || 4096;
-  this.config.monitorGain = config.monitorGain || 0;
-  this.config.numberOfChannels = config.numberOfChannels || 1;
-  this.config.originalSampleRate = this.audioContext.sampleRate;
-  this.config.encoderSampleRate = config.encoderSampleRate || 48000;
-  this.config.encoderPath = config.encoderPath || 'oggopusEncoder.js';
-  this.config.streamPages = config.streamPages || false;
-  this.config.leaveStreamOpen = config.leaveStreamOpen || false;
-  this.config.maxBuffersPerPage = config.maxBuffersPerPage || 40;
-  this.config.encoderApplication = config.encoderApplication || 2049;
-  this.config.encoderFrameSize = config.encoderFrameSize || 20;
-  this.config.streamOptions = config.streamOptions || {
-    optional: [],
-    mandatory: {
-      googEchoCancellation: false,
-      googAutoGainControl: false,
-      googNoiseSuppression: false,
-      googHighpassFilter: false
-    }
-  };
 
   this.state = "inactive";
-  this.eventTarget = document.createDocumentFragment();
-  this.createAudioNodes();
+  this.config = Object.assign({
+    bufferLength: 4096,
+    encoderApplication: 2049,
+    encoderFrameSize: 20,
+    encoderPath: 'encoderWorker.js',
+    encoderSampleRate: 48000,
+    leaveStreamOpen: false,
+    maxBuffersPerPage: 40,
+    mediaTrackConstraints: true,
+    monitorGain: 0,
+    numberOfChannels: 1,
+    recordingGain: 1,
+    resampleQuality: 3,
+    streamPages: false,
+    wavBitDepth: 16,
+  }, config );
+
+  this.initWorker();
 };
 
+
+// Static Methods
 Recorder.isRecordingSupported = function(){
-  return window.AudioContext && navigator.getUserMedia;
+  return AudioContext && global.navigator && global.navigator.mediaDevices && global.navigator.mediaDevices.getUserMedia && global.WebAssembly;
 };
 
-Recorder.prototype.addEventListener = function( type, listener, useCapture ){
-  this.eventTarget.addEventListener( type, listener, useCapture );
-};
 
-Recorder.prototype.audioContext = new window.AudioContext();
-
-Recorder.prototype.clearStream = function() {
+// Instance Methods
+Recorder.prototype.clearStream = function(){
   if ( this.stream ) {
-    this.stream.getTracks().forEach(function ( track ) {
-      track.stop();
-    });
+
+    if ( this.stream.getTracks ) {
+      this.stream.getTracks().forEach( function( track ){
+        track.stop();
+      });
+    }
+
+    else {
+      this.stream.stop();
+    }
+
     delete this.stream;
   }
-};
 
-Recorder.prototype.createAudioNodes = function(){
-  var that = this;
-  this.scriptProcessorNode = this.audioContext.createScriptProcessor( this.config.bufferLength, this.config.numberOfChannels, this.config.numberOfChannels );
-  this.scriptProcessorNode.onaudioprocess = function( e ){
-    that.encodeBuffers( e.inputBuffer );
-  };
-
-  this.monitorNode = this.audioContext.createGain();
-  this.setMonitorGain( this.config.monitorGain );
-
-  if ( this.config.sampleRate < this.audioContext.sampleRate ) {
-    this.createButterworthFilter();
+  if ( this.audioContext ){
+    this.audioContext.close();
+    delete this.audioContext;
   }
-};
-
-Recorder.prototype.createButterworthFilter = function(){
-  this.filterNode = this.audioContext.createBiquadFilter();
-  this.filterNode2 = this.audioContext.createBiquadFilter();
-  this.filterNode3 = this.audioContext.createBiquadFilter();
-  this.filterNode.type = this.filterNode2.type = this.filterNode3.type = "lowpass";
-
-  var nyquistFreq = this.config.sampleRate / 2;
-  this.filterNode.frequency.value = this.filterNode2.frequency.value = this.filterNode3.frequency.value = nyquistFreq - ( nyquistFreq / 3.5355 );
-  this.filterNode.Q.value = 0.51764;
-  this.filterNode2.Q.value = 0.70711;
-  this.filterNode3.Q.value = 1.93184;
-
-  this.filterNode.connect( this.filterNode2 );
-  this.filterNode2.connect( this.filterNode3 );
-  this.filterNode3.connect( this.scriptProcessorNode );
 };
 
 Recorder.prototype.encodeBuffers = function( inputBuffer ){
@@ -94,95 +148,134 @@ Recorder.prototype.encodeBuffers = function( inputBuffer ){
       buffers[i] = inputBuffer.getChannelData(i);
     }
 
-    this.encoder.postMessage({ command: "encode", buffers: buffers });
-    this.duration += inputBuffer.duration;
-    this.eventTarget.dispatchEvent( new CustomEvent( 'duration', { detail: this.duration } ) );
+    this.encoder.postMessage({
+      command: "encode",
+      buffers: buffers
+    });
   }
 };
 
-Recorder.prototype.initStream = function(){
-  if ( this.stream ) {
-    this.eventTarget.dispatchEvent( new Event( "streamReady" ) );
-    return;
+Recorder.prototype.initAudioContext = function( sourceNode ){
+  if (sourceNode && sourceNode.context) {
+    this.audioContext = sourceNode.context;
   }
 
-  var that = this;
-  navigator.getUserMedia(
-    { audio : this.config.streamOptions },
-    function ( stream ) {
-      that.stream = stream;
-      that.sourceNode = that.audioContext.createMediaStreamSource( stream );
-      that.sourceNode.connect( that.filterNode || that.scriptProcessorNode );
-      that.sourceNode.connect( that.monitorNode );
-      that.eventTarget.dispatchEvent( new Event( "streamReady" ) );
-    },
-    function ( e ) {
-      that.eventTarget.dispatchEvent( new ErrorEvent( "streamError", { error: e } ) );
-    }
-  );
+  if ( !this.audioContext ) {
+    this.audioContext = new AudioContext();
+  }
+
+  return this.audioContext;
+};
+
+Recorder.prototype.initAudioGraph = function(){
+  var self = this;
+
+  // First buffer can contain old data. Don't encode it.
+  this.encodeBuffers = function(){
+    delete this.encodeBuffers;
+  };
+
+  this.scriptProcessorNode = this.audioContext.createScriptProcessor( this.config.bufferLength, this.config.numberOfChannels, this.config.numberOfChannels );
+  this.scriptProcessorNode.connect( this.audioContext.destination );
+  this.scriptProcessorNode.onaudioprocess = function( e ) {
+    self.encodeBuffers( e.inputBuffer );
+  };
+
+  this.monitorGainNode = this.audioContext.createGain();
+  this.setMonitorGain( this.config.monitorGain );
+  this.monitorGainNode.connect( this.audioContext.destination );
+
+  this.recordingGainNode = this.audioContext.createGain();
+  this.setRecordingGain( this.config.recordingGain );
+  this.recordingGainNode.connect( this.scriptProcessorNode );
+};
+
+Recorder.prototype.initSourceNode = function( sourceNode ){
+  var self = this;
+
+  if ( sourceNode && sourceNode.context ) {
+    return global.Promise.resolve( sourceNode );
+  }
+
+  if ( this.stream && this.sourceNode ) {
+    return global.Promise.resolve( this.sourceNode );
+  }
+
+  return global.navigator.mediaDevices.getUserMedia({ audio : this.config.mediaTrackConstraints }).then( function( stream ){
+    self.stream = stream;
+    return self.audioContext.createMediaStreamSource( stream );
+  });
+};
+
+Recorder.prototype.initWorker = function(){
+  var self = this;
+  var streamPage = function( e ) { self.streamPage( e.data ); };
+  var storePage = function( e ) { self.storePage( e.data ); };
+
+  this.recordedPages = [];
+  this.totalLength = 0;
+  this.encoder = new global.Worker( this.config.encoderPath );
+  this.encoder.addEventListener( "message", this.config.streamPages ? streamPage : storePage );
 };
 
 Recorder.prototype.pause = function(){
   if ( this.state === "recording" ){
     this.state = "paused";
-    this.eventTarget.dispatchEvent( new Event( 'pause' ) );
+    this.onpause();
   }
-};
-
-Recorder.prototype.removeEventListener = function( type, listener, useCapture ){
-  this.eventTarget.removeEventListener( type, listener, useCapture );
 };
 
 Recorder.prototype.resume = function() {
   if ( this.state === "paused" ) {
     this.state = "recording";
-    this.eventTarget.dispatchEvent( new Event( 'resume' ) );
+    this.onresume();
+  }
+};
+
+Recorder.prototype.setRecordingGain = function( gain ){
+  this.config.recordingGain = gain;
+
+  if ( this.recordingGainNode && this.audioContext ) {
+    this.recordingGainNode.gain.setTargetAtTime(gain, this.audioContext.currentTime, 0.01);
   }
 };
 
 Recorder.prototype.setMonitorGain = function( gain ){
-  this.monitorNode.gain.value = gain;
+  this.config.monitorGain = gain;
+
+  if ( this.monitorGainNode && this.audioContext ) {
+    this.monitorGainNode.gain.setTargetAtTime(gain, this.audioContext.currentTime, 0.01);
+  }
 };
 
-Recorder.prototype.start = function(){
-  if ( this.state === "inactive" && this.stream ) {
-    var that = this;
-    this.encoder = new Worker( this.config.encoderPath );
+Recorder.prototype.start = function( sourceNode ){
+  if ( this.state === "inactive" ) {
+    var self = this;
+    this.initAudioContext( sourceNode );
+    this.initAudioGraph();
 
-    if (this.config.streamPages){
-      this.encoder.addEventListener( "message", function( e ) {
-        that.streamPage( e.data );
-      });
-    }
-
-    else {
-      this.recordedPages = [];
-      this.totalLength = 0;
-      this.encoder.addEventListener( "message", function( e ) {
-        that.storePage( e.data );
-      });
-    }
-
-    // First buffer can contain old data. Don't encode it.
-    this.encodeBuffers = function(){
-      delete this.encodeBuffers;
-    };
-
-    this.duration = 0;
-    this.state = "recording";
-    this.monitorNode.connect( this.audioContext.destination );
-    this.scriptProcessorNode.connect( this.audioContext.destination );
-    this.eventTarget.dispatchEvent( new Event( 'start' ) );
-    this.eventTarget.dispatchEvent( new CustomEvent( 'duration', { detail: this.duration } ) );
-    this.encoder.postMessage( this.config );
+    return this.initSourceNode( sourceNode ).then( function( sourceNode ){
+      self.state = "recording";
+      self.encoder.postMessage( Object.assign({
+        command: 'init',
+        originalSampleRate: self.audioContext.sampleRate,
+        wavSampleRate: self.audioContext.sampleRate
+      }, self.config));
+      self.sourceNode = sourceNode;
+      self.sourceNode.connect( self.monitorGainNode );
+      self.sourceNode.connect( self.recordingGainNode );
+      self.onstart();
+    });
   }
 };
 
 Recorder.prototype.stop = function(){
   if ( this.state !== "inactive" ) {
     this.state = "inactive";
-    this.monitorNode.disconnect();
+    this.monitorGainNode.disconnect();
     this.scriptProcessorNode.disconnect();
+    this.recordingGainNode.disconnect();
+    this.sourceNode.disconnect();
 
     if ( !this.config.leaveStreamOpen ) {
       this.clearStream();
@@ -193,37 +286,75 @@ Recorder.prototype.stop = function(){
 };
 
 Recorder.prototype.storePage = function( page ) {
-  this.recordedPages.push( page );
-  this.totalLength += page.length;
-
-  // Stream is finished
-  if ( page[5] & 4 ) {
+  if ( page === null ) {
     var outputData = new Uint8Array( this.totalLength );
-    var outputIndex = 0;
+    this.recordedPages.reduce( function( offset, page ){
+      outputData.set( page, offset );
+      return offset + page.length;
+    }, 0);
 
-    for ( var i = 0; i < this.recordedPages.length; i++ ) {
-      outputData.set( this.recordedPages[i], outputIndex );
-      outputIndex += this.recordedPages[i].length;
-    }
+    this.ondataavailable( outputData );
+    this.initWorker();
+    this.onstop();
+  }
 
-    this.eventTarget.dispatchEvent( new CustomEvent( 'dataAvailable', {
-      detail: new Blob( [outputData], { type: "audio/ogg" } )
-    }));
-
-    this.recordedPages = [];
-    this.eventTarget.dispatchEvent( new Event( 'stop' ) );
+  else {
+    this.recordedPages.push( page );
+    this.totalLength += page.length;
   }
 };
 
 Recorder.prototype.streamPage = function( page ) {
-  this.eventTarget.dispatchEvent( new CustomEvent( 'dataAvailable', {
-    detail: new Blob( [page], { type: "audio/ogg" } )
-  }));
+  if ( page === null ) {
+    this.initWorker();
+    this.onstop();
+  }
 
-  // Stream is finished
-  if ( page[5] & 4 ) {
-    this.eventTarget.dispatchEvent( new Event( 'stop' ) );
+  else {
+    this.ondataavailable( page );
   }
 };
 
+
+// Callback Handlers
+Recorder.prototype.ondataavailable = function(){};
+Recorder.prototype.onpause = function(){};
+Recorder.prototype.onresume = function(){};
+Recorder.prototype.onstart = function(){};
+Recorder.prototype.onstop = function(){};
+
+
 module.exports = Recorder;
+
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+var g;
+
+// This works in non-strict mode
+g = (function() {
+	return this;
+})();
+
+try {
+	// This works if eval is allowed (see CSP)
+	g = g || Function("return this")() || (1,eval)("this");
+} catch(e) {
+	// This works if the window reference is available
+	if(typeof window === "object")
+		g = window;
+}
+
+// g can still be undefined, but nothing to do about it...
+// We return undefined, instead of nothing here, so it's
+// easier to handle this case. if(!global) { ...}
+
+module.exports = g;
+
+
+/***/ })
+/******/ ]);
+});
